25e4571cdbd5abcfacb60d846e3749605377027d
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-06-05 14:55:20 +0200

    cd..

--- app/src/main/java/com/e/linearlayoutexample/MainActivity.kt
+++ app/src/main/java/com/e/linearlayoutexample/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.e.linearlayoutexample
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -11,5 +12,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         text1.setText("Kolejny napis")
+
+        text1.setBackgroundColor(Color.WHITE)
+        text2.setBackgroundColor(Color.CYAN)
+
+        var kolory = true
+        var liczbaKlikniec = 1
+
+        button0.setOnClickListener {
+            if(kolory) {
+                text1.setBackgroundColor(Color.CYAN)
+                text2.setBackgroundColor(Color.WHITE)
+            }else{
+                text1.setBackgroundColor(Color.WHITE)
+                text2.setBackgroundColor(Color.CYAN)
+            }
+
+            kolory = !kolory
+        }
     }
 }
