7b6924786ee94fea03267e3eb3c63756ebb8ddc9
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-06-05 15:10:02 +0200

    cd..

--- app/src/main/java/com/e/linearlayoutexample/MainActivity.kt
+++ app/src/main/java/com/e/linearlayoutexample/MainActivity.kt
@@ -11,15 +11,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        text1.setText("Kolejny napis")
-
         text1.setBackgroundColor(Color.WHITE)
         text2.setBackgroundColor(Color.CYAN)
 
         var kolory = true
-        var liczbaKlikniec = 1
+        var liczbaKlikniec = 0
+
+        text1.setText("Liczba kliknięć ${liczbaKlikniec}")
 
         button0.setOnClickListener {
+            liczbaKlikniec++
+            text1.setText("Liczba kliknięć ${liczbaKlikniec}")
             if(kolory) {
                 text1.setBackgroundColor(Color.CYAN)
                 text2.setBackgroundColor(Color.WHITE)
